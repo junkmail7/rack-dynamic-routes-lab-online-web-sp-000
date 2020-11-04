c3e743cea97da9af7a06602fe81ee4fc842c4a00
William Pannell <lpann5@gmail.com>
2020-11-04 22:52:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,9 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find do |i|
-        i.name
+        i.name == item_name
+      end
+      resp.write item.price
     else
       resp.write "404"
     end
