08c3d46a4c85742da277fcf4e601157d4188bd01
William Pannell <lpann5@gmail.com>
2020-11-04 22:50:33 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,10 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     if req.path.match(/items/)
+      item_name = req.path.split("/items/").last
+      item = 
+    else
+      resp.write "404"
     end
   end
 end
